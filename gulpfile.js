7f6baa1e7ee937c69be53fba03da6f4d012487c3
kohei <admin@example.com>
2016-01-01 18:03:12 +0900

    画像が読み込めない問題を修正

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,23 @@ var source = require('vinyl-source-stream');
 var webserver = require('gulp-webserver');
 
 gulp.task('browserify', function() {
-    browserify('./client/scripts/app.jsx', { debug: true })
+    browserify('./scripts/app.jsx', { debug: true })
     .transform(babelify)
     .bundle()
     .on("error", function (err) { console.log("Error : " + err.message); })
-    .pipe(source('./client/scripts/build.js'))
+    .pipe(source('./scripts/build.js'))
     .pipe(gulp.dest('./'))
 });
 
 gulp.task('watch', function() {
-  var jsxpath = "./client/scripts/*.jsx"
-  var htmlpath = "./client/*.html"
+  var jsxpath = "./scripts/*.jsx"
+  var htmlpath = "./*.html"
   gulp.watch(jsxpath, ['browserify'])
   gulp.watch(htmlpath)
 });
 
 gulp.task('webserver', function() {
-  gulp.src('./client/')
+  gulp.src('./')
     .pipe(webserver({
       host: '127.0.0.1',
       livereload: true
